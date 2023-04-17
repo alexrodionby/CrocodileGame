fa5251d8229bebb3568ce632d703f103ce044431
Vladimir <motiw@icloud.com>
2023-04-18 02:01:39 +0600

    add game extension

--- CrocodileGame/Screens/Game/GameViewController.swift
+++ CrocodileGame/Screens/Game/GameViewController.swift
@@ -1,29 +1,18 @@
-//
-//  GameViewController.swift
-//  CrocodileGame
-//
-//  Created by Alexandr Rodionov on 16.04.23.
-//
-
 import UIKit
 
 class GameViewController: BaseController {
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+extension GameViewController {
+    override func setupViews() {
+        super.setupViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func layoutViews() {
+        super.layoutViews()
     }
-    */
-
+    
+    override func configureAppearance() {
+        super.configureAppearance()
+    }
 }
