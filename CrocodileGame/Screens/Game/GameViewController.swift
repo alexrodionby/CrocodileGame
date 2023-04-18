97da0c6c6103d675c5c5ffee960e6cd59264a9a1
Vladimir <motiw@icloud.com>
2023-04-18 08:14:12 +0600

    fix game controller

--- CrocodileGame/Screens/Game/GameViewController.swift
+++ CrocodileGame/Screens/Game/GameViewController.swift
@@ -5,70 +5,113 @@ class GameViewController: BaseController {
     private let timerLabel = UILabel()
     private let titleLabel = UILabel()
     private let descriptionLabel = UILabel()
+    private lazy var titleStack = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel])
     private let rightButton = UIButton(type: .system)
     private let wrongButton = UIButton(type: .system)
     private let skipButton = UIButton(type: .system)
-    private lazy var buttonStack = UIStackView(arrangedSubviews: [rightButton, wrongButton, skipButton])
+    private lazy var buttonStack = UIStackView(
+        arrangedSubviews: [rightButton, wrongButton, skipButton])
+    
 }
 
 extension GameViewController {
     override func setupViews() {
         super.setupViews()
-        view.addSubview(logoImageView)
-        view.addSubview(timerLabel)
-        view.addSubview(titleLabel)
-        view.addSubview(descriptionLabel)
-        view.addSubview(buttonStack)
-        logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        timerLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        buttonStack.translatesAutoresizingMaskIntoConstraints = false
-        
+        setupLogoImage()
+        setupTimerLabel()
+        setupTitleLabel()
+        setupDescriptionLabel()
+        setupTitleStack()
+        setupRightButton()
+        setupWrongButton()
+        setupSkipButton()
+        setupButtonStack()
     }
     
-    override func layoutViews() {
-        super.layoutViews()
+    private func setupLogoImage() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            timerLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 57),
-            timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            titleLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            titleLabel.trailingAnchor.constraint(equalToSystemSpacingAfter: view.trailingAnchor, multiplier: -1),
-            
-            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
-            descriptionLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            descriptionLabel.trailingAnchor.constraint(equalToSystemSpacingAfter: view.trailingAnchor, multiplier: -1),
-            
-            buttonStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
-            view.trailingAnchor.constraint(equalTo: buttonStack.trailingAnchor, constant: 12),
-            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalToSystemSpacingBelow: buttonStack.bottomAnchor, multiplier: 1)
         ])
     }
     
-    override func configureAppearance() {
-        super.configureAppearance()
+    private func setupTimerLabel() {
+        view.addSubview(timerLabel)
+        timerLabel.translatesAutoresizingMaskIntoConstraints = false
         timerLabel.text = "00:59"
         timerLabel.font = .italicSystemFont(ofSize: 48)
-        
+        NSLayoutConstraint.activate([
+            timerLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 20),
+            timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func setupTitleLabel() {
+        view.addSubview(titleLabel)
         titleLabel.text = "Картошка"
         titleLabel.textAlignment = .center
         titleLabel.font = .italicSystemFont(ofSize: 48)
-        
+    }
+    
+    private func setupDescriptionLabel() {
+        view.addSubview(descriptionLabel)
         descriptionLabel.text = "объясни с помощью жестов"
         descriptionLabel.textAlignment = .center
         descriptionLabel.font = .italicSystemFont(ofSize: 20)
+    }
+    
+    private func setupTitleStack() {
+        view.addSubview(titleStack)
+        titleStack.translatesAutoresizingMaskIntoConstraints = false
+        titleStack.axis = .vertical
+        NSLayoutConstraint.activate([
+            titleStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            titleStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 11),
+            titleStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -11)
+        ])
+    }
+    
+    private func setupRightButton() {
         rightButton.configure(with: .green, title: "Правильно")
+        rightButton.addTarget(self, action: #selector(rightButtonHandler), for: .primaryActionTriggered)
+    }
+    
+    private func setupWrongButton() {
         wrongButton.configure(with: .red, title: "Нарушил правила")
+        wrongButton.addTarget(self, action: #selector(wrongButtonHandler), for: .primaryActionTriggered)
+    }
+    
+    private func setupSkipButton() {
         skipButton.configure(with: .gray, title: "Сбросить")
-        
+        skipButton.addTarget(self, action: #selector(skipButtonHandler), for: .primaryActionTriggered)
+    }
+    
+    private func setupButtonStack() {
+        view.addSubview(buttonStack)
+        buttonStack.translatesAutoresizingMaskIntoConstraints = false
         buttonStack.axis = .vertical
         buttonStack.alignment = .fill
         buttonStack.distribution = .fillEqually
         buttonStack.spacing = 11
+        NSLayoutConstraint.activate([
+            buttonStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            view.trailingAnchor.constraint(equalTo: buttonStack.trailingAnchor, constant: 12),
+            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalToSystemSpacingBelow:
+                                                                buttonStack.bottomAnchor, multiplier: 1)
+        ])
+    }
+    
+    @objc func rightButtonHandler() {
+        print(#function)
+    }
+    
+    @objc func wrongButtonHandler() {
+        print(#function)
+    }
+    
+    @objc func skipButtonHandler() {
+        print(#function)
     }
 }
