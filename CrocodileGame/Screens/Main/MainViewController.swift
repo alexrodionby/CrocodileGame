95ca0ea6c9a4063a12f8f5432af3f2366fd6ffa6
Olga <olga.shovgeneva@gmail.com>
2023-04-18 17:58:22 +0300

    Added images and buttons on MainViewController

--- CrocodileGame/Screens/Main/MainViewController.swift
+++ CrocodileGame/Screens/Main/MainViewController.swift
@@ -8,10 +8,93 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    let backgroundImageView = UIImageView(image: UIImage(named: "background"))
+    let crocodileImageView = UIImageView(image: UIImage(named: "crocodile"))
+    let leftGrassImageView = UIImageView(image: UIImage(named: "grass"))
+    let rightGrassImageView = UIImageView(image: UIImage(named: "grass"))
+    let startButton = UIButton(type: .system)
+    let rulesButton = UIButton(type: .system)
+    
+    private func setapRulesButton() {
+        view.addSubview(rulesButton)
+        rulesButton.translatesAutoresizingMaskIntoConstraints = false
+        rulesButton.configure(with: .green, title: "Правила")
+        rulesButton.addTarget(self, action: #selector(rulesButtonHandler), for: .primaryActionTriggered)
+        
+        NSLayoutConstraint.activate([
+            rulesButton.topAnchor.constraint(equalTo: startButton.bottomAnchor, constant: 39),
+            rulesButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 83),
+            rulesButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -81),
+            rulesButton.heightAnchor.constraint(equalToConstant: 63)
+        ])
+    }
+    
+    @objc func rulesButtonHandler() {
+        print("pressed button")
+    }
+
+    private func setapStartButton() {
+        view.addSubview(startButton)
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        startButton.configure(with: .green, title: "Старт игры")
+        startButton.addTarget(self, action: #selector(startButtonHandler), for: .primaryActionTriggered)
+        
+        NSLayoutConstraint.activate([
+            startButton.topAnchor.constraint(equalTo: crocodileImageView.bottomAnchor, constant: 48),
+            startButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            startButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -51),
+            startButton.heightAnchor.constraint(equalToConstant: 83)
+        ])
+    }
+    @objc func startButtonHandler() {
+        print("pressed button")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(named: "greenButton")
+        view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        view.addSubview(crocodileImageView)
+        crocodileImageView.translatesAutoresizingMaskIntoConstraints = false
+        crocodileImageView.contentMode = .scaleAspectFit
+        NSLayoutConstraint.activate([
+            crocodileImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 172),
+            crocodileImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 54),
+            crocodileImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -53),
+            crocodileImageView.widthAnchor.constraint(equalToConstant: 268),
+            crocodileImageView.heightAnchor.constraint(equalToConstant: 322)
+        ])
+        
+        view.addSubview(leftGrassImageView)
+        leftGrassImageView.translatesAutoresizingMaskIntoConstraints = false
+        leftGrassImageView.contentMode = .scaleAspectFit
+        NSLayoutConstraint.activate([
+            leftGrassImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            leftGrassImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            leftGrassImageView.widthAnchor.constraint(equalToConstant: 95),
+            leftGrassImageView.heightAnchor.constraint(equalToConstant: 70)
+        ])
+        
+        view.addSubview(rightGrassImageView)
+        rightGrassImageView.translatesAutoresizingMaskIntoConstraints = false
+        rightGrassImageView.contentMode = .scaleAspectFit
+        NSLayoutConstraint.activate([
+            rightGrassImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            rightGrassImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            rightGrassImageView.widthAnchor.constraint(equalToConstant: 95),
+            rightGrassImageView.heightAnchor.constraint(equalToConstant: 70)
+        ])
+        
+        setapStartButton()
+        setapRulesButton()
+        
     }
     
 
