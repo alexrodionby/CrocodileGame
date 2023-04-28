f5f365078e44ffb5707a99688c553e32d0d15206
Anton Medovnik <krimeainbox@yandex.ru>
2023-04-28 17:06:43 +0300

    If team placeholder is empty - the new team cell will not add to table view

--- CrocodileGame/Screens/Team/TeamViewController.swift
+++ CrocodileGame/Screens/Team/TeamViewController.swift
@@ -6,21 +6,21 @@ class TeamViewController: BaseController {
     let model = TeamModel()
     var teams: [Team]
     let tableView = UITableView()
-
-  
     
     
-
+    
+    
+    
     init(numberOfTeam: Int) {
         teams = model.randomTeams(count: numberOfTeam)
         super.init(nibName: nil, bundle: nil)
     }
-   
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-        
+    
     override func setupViews() {
         super.setupViews()
         setupGreenButton("Игроки готовы")
@@ -44,7 +44,7 @@ class TeamViewController: BaseController {
             tableView.bottomAnchor.constraint(equalTo: greenButton.topAnchor, constant: -80)
         ])
     }
-        
+    
     override func greenButtonHandler() {
         let controller = CategoryViewController(teams: teams)
         navigationController?.pushViewController(controller, animated: true)
@@ -54,24 +54,24 @@ class TeamViewController: BaseController {
         view.addSubview(addTeamButton)
         addTeamButton.translatesAutoresizingMaskIntoConstraints = false
         addTeamButton.configure(with: .green,
-                              title: title,
-                              height: 63)
+                                title: title,
+                                height: 63)
         addTeamButton.addTarget(self,
-                              action: #selector(addNewTeam),
-                              for: .primaryActionTriggered)
+                                action: #selector(addNewTeam),
+                                for: .primaryActionTriggered)
         
         
         NSLayoutConstraint.activate([
             addTeamButton.bottomAnchor.constraint(equalTo: greenButton.topAnchor,
-                                                constant: -14),
+                                                  constant: -14),
             addTeamButton.leadingAnchor.constraint(equalTo: view.leadingAnchor,
-                                                 constant: 14),
+                                                   constant: 14),
             addTeamButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,
-                                                  constant: -14)
+                                                    constant: -14)
         ])
     }
     @objc func addNewTeam () {
-//        var teams: [Team]
+        //        var teams: [Team]
         
         
         let alertController = UIAlertController(title: "Введите название команды", message: "", preferredStyle: UIAlertController.Style.alert)
@@ -83,6 +83,8 @@ class TeamViewController: BaseController {
             newTeamCrodile = ""
             
             
+            
+            
         })
         alertController.addTextField { (textField) in
             textField.placeholder = "Название команды"
@@ -91,11 +93,16 @@ class TeamViewController: BaseController {
     }
     
     
-    @objc func addNewTeamHandler () {
-        let allImages = model.allImages.randomElement()!
-        let newIndexPath = IndexPath (row: newTeamCrodile.count, section: 0)
-        self.teams.append(Team(name: newTeamCrodile, image: allImages ))
-        tableView.reloadData()
+    @objc func addNewTeamHandler () -> Bool {
+        if newTeamCrodile.count > 0 {
+            let allImages = model.allImages.randomElement()!
+            let newIndexPath = IndexPath (row: newTeamCrodile.count, section: 0)
+            self.teams.append(Team(name: newTeamCrodile, image: allImages ))
+            
+            tableView.reloadData()}
+        
+        
+        return false
     }
 }
 
